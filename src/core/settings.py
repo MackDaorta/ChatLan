544bbbe6592c80074bdf5ff832322c3b5272956c
Mack <daortamack@gmail.com>
2025-11-24 18:11:54 -0500

    inicio

--- src/core/settings.py
+++ src/core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-us!$do9pc=gte2gel05lv%1s5ke4zy-(^iv+^#i+i_4n6*(94i'
+SECRET_KEY = os.environ.get('SECRET_KEY','fall-back-key-solo-para-decv')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG') == 'True'
+
+
 
 ALLOWED_HOSTS = ['*']
 
